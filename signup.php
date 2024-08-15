762a294c988774a05a72aa31c432916c2417bde3
Ruthresh <ruthreshprabhakaran@gmail.com>
2024-08-15 18:22:29 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Animated Login Form</title>
+	<link rel="stylesheet" href="signup.css">
+</head>
+<body>
+	<div class="box">
+		<form autocomplete="off">
+			<h2>Sign Up</h2>
+			<div class="inputBox">
+				<input type="text" required="required">
+				<span>First Name</span>
+				<i></i>
+			</div>
+			<div class="inputBox">
+				<input type="text" required="required">
+				<span>Last Name</span>
+				<i></i>
+			</div>
+            <div class="inputBox">
+				<input type="email" required="required">
+				<span>Email</span>
+				<i></i>
+			</div>
+			<div class="inputBox">
+				<input type="password" required="required">
+				<span> New Password</span>
+				<i></i>
+			</div>
+			<div class="links">
+				<a href="login.html">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;already a customer click here to login</a>
+			</div>
+			
+			<input type="submit" value="Sign Up">
+			<div class="links">
+				<a href="#">&nbsp;&nbsp;&nbsp;By creating an account, I accept the Terms & &nbsp;&nbsp;&nbsp;Conditions & Privacy Policy</a>
+			</div>
+			
+		</form>
+	</div>
+</body>
+</html>
